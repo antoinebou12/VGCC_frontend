24a079c2170f9274273adc086d977891dae9ef43
Valentin387 <valentin.valencia@utp.edu.co>
2024-04-03 22:57:35 -0500

    toggle in navbar items done

--- vgcc_frontend/src/app/navbar/navbar.component.ts
+++ vgcc_frontend/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';  // Import MatSelectModule
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +10,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  selectedItem: string = '';
 
+  constructor(private router: Router) { }  // Inject Router
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) { // Check for NavigationEnd event
+        this.selectedItem = event.urlAfterRedirects; // Update selectedItem
+      }
+    });
+  }
 }
