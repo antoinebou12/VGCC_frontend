98f06904339f4d7a474e49f6a403c5d8fa2f2bbc
Valentin387 <valentin.valencia@utp.edu.co>
2024-04-03 21:28:59 -0500

    title changed

--- vgcc_frontend/src/app/app.component.ts
+++ vgcc_frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MatSelectModule } from '@angular/material/select'; // Import MatSelectModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MatSelectModule],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
