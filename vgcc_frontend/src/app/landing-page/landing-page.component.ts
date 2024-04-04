0a33cac07ca138f42e86c5014c6924c8d0e1ee04
ValentinVV42 <valentin.valencia.1@ens.etsmtl.ca>
2024-04-04 17:37:03 -0500

    work in landing page

--- vgcc_frontend/src/app/landing-page/landing-page.component.ts
+++ vgcc_frontend/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatExpansionModule], // Include CommonModule here
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css'] // Correct the property name to styleUrls
 })
 export class LandingPageComponent {
-
+  // Declare the users property
+  users: any[] = [
+    { name: 'User 1', description: 'Description of User 1' },
+    { name: 'User 2', description: 'Description of User 2' },
+    // Add more users as needed
+  ];
 }
