fb5f0e5670c9011b6ba982b3789f60822fe069af
Valentin387 <valentin.valencia@utp.edu.co>
2024-04-03 22:35:59 -0500

    routing fixed

--- vgcc_frontend/src/app/app.component.ts
+++ vgcc_frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- vgcc_frontend/src/app/navbar/navbar.component.ts
+++ vgcc_frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';  // Import MatSelectModule
+import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatSelectModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
