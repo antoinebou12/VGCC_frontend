4e05c73d12e6fab33f4bcc44adb4772d32cf19f5
Valentin387 <valentin.valencia@utp.edu.co>
2024-04-03 21:23:08 -0500

    first successful run

--- vgcc_frontend/src/app/app.routes.ts
+++ vgcc_frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: '', component: LandingPageComponent },
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
--- vgcc_frontend/src/app/navbar/navbar.component.ts
+++ vgcc_frontend/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';  // Import MatSelectModule
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
