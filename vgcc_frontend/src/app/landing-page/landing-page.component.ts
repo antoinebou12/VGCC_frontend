70a3819577b28c314cc209b8425cc55c69b5e379
ValentinVV42 <valentin.valencia.1@ens.etsmtl.ca>
2024-04-06 15:51:24 -0500

    button added

--- vgcc_frontend/src/app/landing-page/landing-page.component.ts
+++ vgcc_frontend/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule], // Include CommonModule here
+  imports: [CommonModule, MatExpansionModule, FormsModule ], // Include CommonModule here
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'] // Correct the property name to styleUrls
 })
@@ -43,5 +44,17 @@ export class LandingPageComponent {
     this.chatMessages.push({ sender: 'chatGPT', message: 'Hi there!', senderType: 'receiver' });
     this.chatMessages.push({ sender: 'You', message: 'Hello!', senderType: 'sender' });
     this.chatMessages.push({ sender: 'chatGPT', message: 'Hi there!', senderType: 'receiver' });
+  }
+
+  @Input() newMessage! : string;
+
+  sendMessage() {
+
+    if (this.newMessage.trim() !== '') { // Ensure message is not empty
+      // Add new message to chatMessages array with sender as "sender"
+      this.chatMessages.push({ sender: 'Sender', message: this.newMessage, senderType: 'sender' });
+      // Clear input box after sending message
+      this.newMessage = '';
+    }
   }
 }
