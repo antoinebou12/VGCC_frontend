8329ce66e183bc7c93b3713b0a98da2c92eabd6b
Valentin387 <valentin.valencia@utp.edu.co>
2024-04-06 18:21:34 -0500

    input changed by textarea

--- vgcc_frontend/src/app/landing-page/landing-page.component.ts
+++ vgcc_frontend/src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule, FormsModule ], // Include CommonModule here
+  imports: [CommonModule, MatExpansionModule, FormsModule, MatIconModule ], // Include CommonModule here
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'] // Correct the property name to styleUrls
 })
@@ -52,9 +53,25 @@ export class LandingPageComponent {
 
     if (this.newMessage.trim() !== '') { // Ensure message is not empty
       // Add new message to chatMessages array with sender as "sender"
-      this.chatMessages.push({ sender: 'Sender', message: this.newMessage, senderType: 'sender' });
+      this.chatMessages.push({ sender: 'You', message: this.newMessage, senderType: 'sender' });
       // Clear input box after sending message
       this.newMessage = '';
+    }
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.sendMessage();
+    }
+  }
+
+  onShiftEnter(event: KeyboardEvent) {
+    const keyboardEvent = event as KeyboardEvent; // Cast event to KeyboardEvent
+    if (keyboardEvent.key === 'Enter' && keyboardEvent.shiftKey) {
+      event.preventDefault(); // Prevent default behavior of newline insertion
+      const textarea = event.target as HTMLTextAreaElement;
+      textarea.style.height = 'auto'; // Auto resize
+      textarea.style.height = (textarea.scrollHeight + 2) + 'px'; // Adjust height
     }
   }
 }
